b7f66c5eed02280f72dba4264ebc6f59cf65e949
GiantGlint <olibert@outlook.com>
2022-03-21 18:13:53 +1100

    make gtk and make worse

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/somemonkers/kapok
 
 go 1.18
+
+require (
+	github.com/diamondburned/gotk4 v0.0.2 // indirect
+	github.com/diamondburned/gotk4/pkg v0.0.0-20220305042118-cfb7981704dc // indirect
+	gitlab.gnome.org/GNOME/gobject-introspection v0.0.0-20220318115602-722cb7431bc1 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20201222180813-1025295fd063 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
--- kapok.go
+++ kapok.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"github.com/diamondburned/gotk4/pkg/gtk/v3"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Kapok:")
-	fmt.Println("your password is _")
-	fmt.Println("(C) (R) Some Monkers Org.")
+	fmt.Println("bruh")
+	
+	label := gtk.NewLabel("Kapok")
+	
+	label.Show()
+
+	window := gtk.NewWindow(gtk.WindowToplevel)
+	//window.ConnectDestroy(gtk.MainQuit)
+	window.SetTitle("Kapok")
+	window.Add(label)
+	window.SetDefaultSize(400, 300)
+	window.Show()
+
+	gtk.Main()
 }
